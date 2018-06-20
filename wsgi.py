779f912f850eede02460f4484e2d605e7d9cd28b
Rikako11 <ro99@cornell.edu>
2018-06-20 02:18:51 -0400

    Changed requirements.txt

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,7 @@
-from whitenoise.django import DjangoWhiteNoise 
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xxxx.settings")
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
